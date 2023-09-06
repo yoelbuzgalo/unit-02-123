2743bb0519ceb638d332327c353f9eff72031809
Yoel Buzgalo (RIT Student) <ybb5956@rit.edu>
2023-09-06 11:49:33 -0400

    Activity 2.13

---
+++ happy_birthday.py
@@ -0,0 +1,6 @@
+name = input("Enter your name: ")
+birth_month = input("Enter your birth month: ")
+birth_day = input("Enter your birth day: ")
+birth_year = input("Enter your birth year: ")
+
+print(name, 'your birthday is on', birth_month, birth_day, ',', birth_year)
