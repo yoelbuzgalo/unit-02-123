8e9505dc5e18c88da9f3f63902769a7f23c33d7d
Yoel Buzgalo (RIT Student) <ybb5956@rit.edu>
2023-09-08 10:40:31 -0400

    Activity 2.17

--- hello.py
+++ hello.py
@@ -1,9 +1,20 @@
+'''
+This program prints out hello world and prompts you to enter your name,
+written by Yoel
+'''
+
 def hello_world():
-    print("Hello, world!")
+    """
+    This function prints out "Hello world!" to the output
+    """
+    print("Hello, world!")# this prints "Hello world! to terminal"
 
 def hello_you():
-    your_name = input("Enter your name: ")
-    print("Hello,", your_name)
+    """
+    This function prompts you to enter your name and will print"Hello,  yourname"
+    """
+    your_name = input("Enter your name: ") # Ask the user its name
+    print("Hello,", your_name) # Prints hello, name
 
 hello_world()
 hello_you()
