4ad87798a30cf8be80fedc25633671292a339c99
Yoel Buzgalo (RIT Student) <ybb5956@rit.edu>
2023-09-08 10:54:23 -0400

    Activity 2.18

---
+++ variables.py
@@ -0,0 +1,13 @@
+'''
+This function prints out variable practices, different kinds of variables part of Activity 2.17
+'''
+
+def variable_practice():
+    my_age = 23*12 # my age variable, times with 12 as in months per year
+    number_of_days_in_year = 365 # number of days in a year
+    pet_name = "Curry" # my pet name
+    pi_value = 3.1475 # pi value
+
+    print("Your age is", my_age, "months! The number of days in a year is", number_of_days_in_year, "and your pet name is", pet_name, "and pi is", pi_value) # prints out all of the variables values
+
+variable_practice() # calls the function
