21672748464460ed3cb94ef7f90338282e93cdb4
Yoel Buzgalo (RIT Student) <ybb5956@rit.edu>
2023-09-11 10:40:12 -0400

    Activity 2.26

--- ascii.py
+++ ascii.py
@@ -1,3 +1,5 @@
+A_ASCII_VALUE = ord("A")
+
 def convert_to_ascii(character):
     my_ord = ord(character)
     print("The ASCII code of:", character, "is",my_ord)
@@ -6,6 +8,11 @@ def convert_from_ascii(ascii_value):
     my_char = chr(ascii_value)
     print("Ascii value of:", ascii_value, "is", my_char)
 
+def alphabet_position(letter):
+    ascii_value = ord(letter)
+    position = ascii_value - A_ASCII_VALUE + 1
+    print("The letter's position is: ", position, "in the alphabet")
+
 
 def main():
     letter = input("Enter a character:" )
@@ -13,5 +20,8 @@ def main():
     convert_from_ascii(1)
     convert_from_ascii(97)
     convert_from_ascii(100)
+    alphabet_position(letter)
+    alphabet_position(letter)
+    alphabet_position(letter)
 
 main()
