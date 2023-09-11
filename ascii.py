620e8f10151f64a4f21a20ca9ef21562ae0a4b6a
Yoel Buzgalo (RIT Student) <ybb5956@rit.edu>
2023-09-11 10:49:59 -0400

    Activity 2.27

--- ascii.py
+++ ascii.py
@@ -8,20 +8,20 @@ def convert_from_ascii(ascii_value):
     my_char = chr(ascii_value)
     print("Ascii value of:", ascii_value, "is", my_char)
 
-def alphabet_position(letter):
+def alphabet_position(letter, reference):
     ascii_value = ord(letter)
-    position = ascii_value - A_ASCII_VALUE + 1
+    position = ascii_value - reference + 1
     print("The letter's position is: ", position, "in the alphabet")
 
 
 def main():
-    letter = input("Enter a character:" )
-    convert_to_ascii(letter)
-    convert_from_ascii(1)
-    convert_from_ascii(97)
-    convert_from_ascii(100)
-    alphabet_position(letter)
-    alphabet_position(letter)
-    alphabet_position(letter)
+    # letter = input("Enter a character:" )
+    # convert_to_ascii(letter)
+    # convert_from_ascii(1)
+    # convert_from_ascii(97)
+    # convert_from_ascii(100)
+    alphabet_position("A", A_ASCII_VALUE)
+    alphabet_position("B", ord("B"))
+    alphabet_position("C", ord("B"))
 
 main()
