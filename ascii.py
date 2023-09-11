377a1898022877ee60fb10a5c604a5c6c87e0ac0
Yoel Buzgalo (RIT Student) <ybb5956@rit.edu>
2023-09-11 10:16:31 -0400

    Activity 2.24

--- ascii.py
+++ ascii.py
@@ -3,7 +3,16 @@ def convert_to_ascii():
     my_ord = ord(my_char)
     print(my_ord)
 
+def convert_from_ascii():
+    my_int = int(input("Enter an integer: "))
+    my_char = chr(my_int)
+    print("Ascii Code of your char is: ", my_char)
+
+
 def main():
     convert_to_ascii()
+    convert_from_ascii()
+    convert_from_ascii()
+    convert_from_ascii()
 
 main()
